88abbecf20b0ebc74218f34ab18f81d43017c330
ATUL SHARMA <ATUL SHARMA@DESKTOP-HMLGV3R>
2024-08-13 10:03:48 +0530

    Product new element added

--- CRMGUIFramework/src/main/java/com/comcast/crm/Objectrepositoryutlity/CreateProduct.java
+++ CRMGUIFramework/src/main/java/com/comcast/crm/Objectrepositoryutlity/CreateProduct.java
@@ -1,6 +1,11 @@
 package com.comcast.crm.Objectrepositoryutlity;
 
 public class CreateProduct {
+	@FindBy(xpath="//img[@src='themes/softed/images/btnL3Add.gif']")
+	private WebElement CreatenewProductlink;
+	@FindBy(name="search")
+	private WebElement searchbtn ;
+	//nnjn
 	
 
 }
